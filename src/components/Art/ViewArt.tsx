4b6b15001860816bae0bf30b99f687c9f500c427
Legaz <worldoflegaz@icloud.com>
2025-06-09 23:26:13 +0900

    finished

--- src/components/Art/ViewArt.tsx
+++ src/components/Art/ViewArt.tsx
@@ -12,6 +12,7 @@ const ViewArt = () => {
   const [currentIndex, setCurrentIndex] = useState(
     initialIndex === -1 ? 0 : initialIndex
   );
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleNext = () => {
     setCurrentIndex((prev) => (prev + 1) % data.length);
@@ -26,23 +27,28 @@ const ViewArt = () => {
   return (
     <>
       <Navbar />
-      <div className="flex animate-fadeIn justify-between items-center max-w-[1250px] mx-auto mt-6 px-4">
-        <div className="relative max-w-[50%]">
+      <div className="flex flex-col xl:flex-row animate-fadeIn justify-between items-center xl:max-w-[1250px] w-full mx-auto mt-6 px-4">
+        <div className="relative max-w-[100%] xl:max-w-[50%] flex justify-center items-center md:mb-10">
           <img
             src={art.images.hero.large.replace("./", "/")}
             alt={art.name}
-            className="max-w-[500px]"
+            className="max-w-[500px] sm:w-[400px]"
           />
-          <div className="flex flex-col absolute top-0 -right-85 bg-white  h-62 pl-14 pt-1">
-            <h1 className="text-6xl font-bold font-serif w-[350px]">{art.name}</h1>
+          <div className="flex flex-col  absolute top-0 -right-85 bg-white  h-62 pl-14 pt-1">
+            <h1 className="text-6xl font-bold font-serif w-[350px]">
+              {art.name}
+            </h1>
             <p className="text-gray-500">{art.artist.name}</p>
           </div>
           <img
             src={art.artist.image.replace("./", "/")}
             alt={art.artist.name}
-            className="object-contain absolute -right-40 -bottom-10"
+            className="object-contain absolute -right-40 -bottom-10 "
           />
-          <div className="absolute gap-3 flex bottom-5 left-8 bg-black text-white/50 p-2 cursor-pointer hover:bg-white/20">
+          <div
+            className="absolute gap-3 flex bottom-5 left-8 bg-black text-white/50 p-2 cursor-pointer hover:bg-white/20 "
+            onClick={() => setIsModalOpen(true)}
+          >
             <img
               src="/assets/shared/icon-view-image.svg"
               alt="icon-view"
@@ -52,14 +58,14 @@ const ViewArt = () => {
           </div>
         </div>
 
-        <div className="relative max-w-[50%] ml-50 font-serif text-gray-500 font-[500] mt-35">
+        <div className="relative w-full lg:max-w-[50%] ml-50 font-serif text-gray-500 font-[500] mt-35">
           <div className="absolute -top-10 -left-4 md:-left-8 lg:-top-45 lg:-left-10 opacity-10 z-0">
             <span className="text-[200px] md:text-[250px] lg:text-[200px] font-bold">
               {art.year}
             </span>
           </div>
 
-          <div className="flex flex-col gap-5 w-[350px]">
+          <div className="flex flex-col gap-5 w-full xl:w-[350px]">
             <p>{art.description}</p>
             <a
               href={art.source}
@@ -71,7 +77,23 @@ const ViewArt = () => {
             </a>
           </div>
         </div>
-      </div>
+        </div>
+        {isModalOpen && (
+        <div className="fixed inset-0 p-10  bg-black/75 flex justify-center items-center z-50 overflow-auto">
+          <img
+            src={art.images.hero.large.replace("./", "/")}
+            alt="Full art"
+            className="max-w-full max-h-full object-contain"
+          />
+          <button
+            className="absolute top-5 right-5 text-white text-2xl cursor-pointer"
+            onClick={() => setIsModalOpen(false)}
+            aria-label="Close image modal"
+          >
+            &times;
+          </button>
+        </div>
+        )}
       <Progression_bar
         art={art}
         onNext={handleNext}
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,44 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import data from "../../../starter-code/data.json";
 
 const Navbar = () => {
+  const location = useLocation();
+
+  const isGalleryPage = location.pathname === "/";
+
+  const firstArtName = encodeURIComponent(data[0].name);
   return (
     <>
       <nav className="w-full bg-white py-6">
+        {/* Centered content container */}
         <div className="max-w-[1340px] mx-auto px-4 flex justify-between items-center">
           <Link to="/">
-            <img src="/assets/shared/logo.svg" alt="logo" className="h-12" />
+            <img
+              src="/assets/shared/logo.svg"
+              alt="logo"
+              className="md:h-9 lg:h-12"
+            />
           </Link>
-          <Link to="/art">
-          <button className="text-sm md:text-base text-gray-400 hover:text-black hover:underline uppercase tracking-wide transition">
-            Start slideshow
-          </button>
-          </Link>
+
+          {isGalleryPage ? (
+            <Link to={`/art/${firstArtName}`}>
+              <button className="text-sm md:text-base text-gray-400 hover:text-black hover:underline uppercase tracking-wide transition">
+                Start slideshow
+              </button>
+            </Link>
+          ) : (
+            <Link to="/">
+              <button className="text-sm md:text-base text-gray-400 hover:text-black hover:underline uppercase tracking-wide transition">
+                Return to gallery
+              </button>
+            </Link>
+          )}
         </div>
+
+        {/* Full-width horizontal line */}
+        <hr className="border-t border-gray-200 w-full  xl:w-[1340px] mx-auto mt-10" />
       </nav>
       {/* thin divider */}
-      <hr className="border-t border-gray-200 max-w-[1310px] mx-auto" />
     </>
   );
 };
